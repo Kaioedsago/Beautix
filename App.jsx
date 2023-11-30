4980d6fbcd02319a3120c5272abeca1a6a7b51aa
kaioedsago <kaioedsago@gmail.com>
2023-11-30 10:34:59 -0300

    First Commit

---
+++ App.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+
+function App(){
+
+  return (
+    <SafeAreaView>
+        <View>
+         <Text>H22i</Text>
+        </View>
+    </SafeAreaView>
+  );
+}
+
+
+export default App;
